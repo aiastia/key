2dc481a38afb33f559d7fc6889462f939b02a0f5
未来 <aiastia@users.noreply.github.com>
2017-05-21 10:49:47 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
-wget -N --no-check-certificate https://raw.githubusercontent.com/aiastia/us.pub/master/us.pub
+wget -N --no-check-certificate https://raw.githubusercontent.com/aiastia/key/master/us.pub
 
 mkdir -p /root/.ssh 
 
